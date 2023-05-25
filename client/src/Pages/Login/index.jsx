8a2f693990710e319357be7b248ef7aac13f8921
allancassio <allan.cassio1@gmail.com>
2023-05-24 22:53:32 -0300

    styling login and register screen

--- client/src/Pages/Login/index.jsx
+++ client/src/Pages/Login/index.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { RiArrowGoBackFill } from "react-icons/ri";
 
 export function Login() {
   const [isLogin, setIsLogin] = useState(true);
@@ -24,18 +25,27 @@ export function Login() {
             <label htmlFor="password">Senha</label>
             <button type="submit">Entrar</button>
           </form>
-          <span id="button__go--login" onClick={() => setIsLogin(!isLogin)}>
+          <span id="button__goRegister" onClick={() => setIsLogin(!isLogin)}>
             ou Cadastre-se
           </span>
         </div>
       ) : (
-        <form className="form form__login__register" onSubmit={registerUser}>
-          <input type="text" name="username" id="username" />
-          <label htmlFor="username">Nome do usuário</label>
-          <input type="password" name="password" id="password" />
-          <label htmlFor="password">Senha</label>
-          <button type="submit">Cadastrar</button>
-        </form>
+        <div>
+          <form className="form form__login__register" onSubmit={registerUser}>
+            <span className="container__icon">
+              <RiArrowGoBackFill
+                size={"1.5rem"}
+                className="icon"
+                onClick={() => setIsLogin(!isLogin)}
+              />
+            </span>
+            <input type="text" name="username" id="username" />
+            <label htmlFor="username">Nome do usuário</label>
+            <input type="password" name="password" id="password" />
+            <label htmlFor="password">Senha</label>
+            <button type="submit">Cadastrar</button>
+          </form>
+        </div>
       )}
     </main>
   );
